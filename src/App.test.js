e4a2c60c8f36e84ef63cca951d9347f44ebdf7e4
JASPREET SINGH <jpsingh4340@gmail.com>
2026-03-17 15:44:43 +1300

    added:

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
-
+jest.mock('./firebase');
 // Simple test that should always pass
 test('renders app without crashing', () => {
   const { container } = render(<App />);
